778c31722b29e5f2149b0bdf58451dd740539e3a
Guillaume Nodet <gnodet@apache.org>
2008-02-28 23:48:59 +0000

    SMX4NMR-12: endpoints should be registered in the OSGi registry and not directly in the NMR
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/nmr/trunk@632172 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jbi/osgi/src/main/java/org/apache/servicemix/jbi/osgi/NMRWrapper.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicemix.jbi.osgi;
+
+import org.apache.servicemix.nmr.api.Channel;
+import org.apache.servicemix.nmr.api.EndpointRegistry;
+import org.apache.servicemix.nmr.api.NMR;
+import org.apache.servicemix.nmr.api.event.ListenerRegistry;
+import org.apache.servicemix.nmr.api.internal.FlowRegistry;
+import org.osgi.framework.BundleContext;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.osgi.context.BundleContextAware;
+
+/**
+ * A NMR wrapper which purpose is to delegate to the OSGi registry
+ * instead of registering the endpoints directly.
+ * This way, the JBI layer is decoupled from OSGi, while still
+ * having the endpoints registered in the OSGi registry.
+ */
+public class NMRWrapper implements NMR, BundleContextAware, InitializingBean {
+
+    private NMR nmr;
+    private BundleContext bundleContext;
+    private EndpointRegistry registry;
+
+    public void setNmr(NMR nmr) {
+        this.nmr = nmr;
+    }
+
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        if (this.nmr == null) {
+            throw new IllegalArgumentException("nmr must be set");
+        }
+        if (this.bundleContext == null) {
+            throw new IllegalArgumentException("bundleContext must be set");
+        }
+        this.registry = new RegistryWrapper(nmr.getEndpointRegistry(), bundleContext);
+    }
+
+    public EndpointRegistry getEndpointRegistry() {
+        return registry;
+    }
+
+    public ListenerRegistry getListenerRegistry() {
+        return nmr.getListenerRegistry();
+    }
+
+    public FlowRegistry getFlowRegistry() {
+        return nmr.getFlowRegistry();
+    }
+
+    public Channel createChannel() {
+        return nmr.createChannel();
+    }
+
+
+}
---
+++ jbi/osgi/src/main/java/org/apache/servicemix/jbi/osgi/RegistryWrapper.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicemix.jbi.osgi;
+
+import java.util.Map;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.w3c.dom.Document;
+
+import org.apache.servicemix.nmr.api.EndpointRegistry;
+import org.apache.servicemix.nmr.api.Endpoint;
+import org.apache.servicemix.nmr.api.Reference;
+import org.apache.servicemix.nmr.core.util.MapToDictionary;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * A wrapper around an EndpointRegistry.
+ * Instead of directly registering endpoints in the
+ * delegate registry, this one will register the
+ * endpoints in the OSGi registry.
+ *
+ * @see NMRWrapper
+ *
+ */
+public class RegistryWrapper implements EndpointRegistry {
+    private EndpointRegistry registry;
+    private BundleContext bundleContext;
+    private Map<Endpoint, ServiceRegistration> registrations = new ConcurrentHashMap();
+
+    public RegistryWrapper(EndpointRegistry registry, BundleContext bundleContext) {
+        this.registry = registry;
+        this.bundleContext = bundleContext;
+    }
+
+    public void register(Endpoint endpoint, Map<String, ?> properties) {
+        ServiceRegistration reg = bundleContext.registerService(
+                                      Endpoint.class.getName(),
+                                      endpoint,
+                                      new MapToDictionary(properties));
+        registrations.put(endpoint, reg);
+    }
+
+    public void unregister(Endpoint endpoint, Map<String, ?> properties) {
+        ServiceRegistration reg = registrations.remove(endpoint);
+        reg.unregister();
+    }
+
+    public List<Endpoint> query(Map<String, ?> properties) {
+        return registry.query(properties);
+    }
+
+    public Reference lookup(Map<String, ?> properties) {
+        return registry.lookup(properties);
+    }
+
+    public Reference lookup(Document xml) {
+        return registry.lookup(xml);
+    }
+
+    public Reference lookup(String filter) {
+        return registry.lookup(filter);
+    }
+
+    public Map<String, ?> getProperties(Endpoint service) {
+        return registry.getProperties(service);
+    }
+
+    public Set<Endpoint> getServices() {
+        return registry.getServices();
+    }
+}
