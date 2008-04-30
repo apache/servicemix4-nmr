bc8d0378daa0250629e977b8f57d99255605e07d
Guillaume Nodet <gnodet@apache.org>
2008-04-30 15:36:26 +0000

    Add ASF headers and add missing bundle in the feature
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/nmr/trunk@652401 13f79535-47bb-0310-9956-ffa450edef68

--- nmr/core/src/main/java/org/apache/servicemix/nmr/core/Converter.java
+++ nmr/core/src/main/java/org/apache/servicemix/nmr/core/Converter.java
@@ -1,12 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.servicemix.nmr.core;
 
 /**
- * Created by IntelliJ IDEA.
-* User: gnodet
-* Date: Feb 21, 2008
-* Time: 10:42:38 PM
-* To change this template use File | Settings | File Templates.
-*/
+ * Allow pluggable conversion strategies
+ */
 interface Converter {
+
+    /**
+     * Convert the body to the given class
+     * @param body the object to convert
+     * @param type the expected type
+     * @return a converted object or null
+     */
     <T> T convert(Object body, Class<T> type);
+
 }
