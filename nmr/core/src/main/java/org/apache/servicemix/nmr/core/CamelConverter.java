d1fec5e65cbe46210a4ec919d3c4f1ed823dd95a
Hiram R. Chirino <chirino@apache.org>
2008-06-04 19:36:01 +0000

    Fix camel compile error.  Packages were moved around.
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/nmr/trunk@663353 13f79535-47bb-0310-9956-ffa450edef68

--- nmr/core/src/main/java/org/apache/servicemix/nmr/core/CamelConverter.java
+++ nmr/core/src/main/java/org/apache/servicemix/nmr/core/CamelConverter.java
@@ -16,14 +16,15 @@
  */
 package org.apache.servicemix.nmr.core;
 
+import org.apache.camel.impl.converter.DefaultTypeConverter;
+import org.apache.camel.util.ReflectionInjector;
+
 /**
  * A converter using Camel underneath
  */
 class CamelConverter implements Converter {
 
-    final org.apache.camel.impl.converter.DefaultTypeConverter tc =
-            new org.apache.camel.impl.converter.DefaultTypeConverter(
-                    new org.apache.camel.impl.ReflectionInjector());
+    final DefaultTypeConverter tc = new DefaultTypeConverter(new ReflectionInjector());
 
     public <T> T convert(Object body, Class<T> type) {
         return tc.convertTo(type, body);
