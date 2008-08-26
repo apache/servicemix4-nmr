6c948aecd952d99d5da232412365782a15af29a4
Guillaume Nodet <gnodet@apache.org>
2008-08-26 08:16:26 +0000

    SM-1455: Update NMR with the new JBI spec
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/nmr/trunk@688991 13f79535-47bb-0310-9956-ffa450edef68

--- jbi/itests/src/test/java/org/apache/servicemix/jbi/IntegrationTest.java
+++ jbi/itests/src/test/java/org/apache/servicemix/jbi/IntegrationTest.java
@@ -57,6 +57,7 @@ public class IntegrationTest extends AbstractIntegrationTest {
 	protected String[] getTestBundlesNames() {
         return new String[] {
             getBundle("org.apache.servicemix.specs", "org.apache.servicemix.specs.stax-api-1.0"),
+            getBundle("org.apache.servicemix.specs", "org.apache.servicemix.specs.jbi-api-1.0"),
             getBundle("org.apache.geronimo.specs", "geronimo-activation_1.1_spec"),
             getBundle("org.apache.geronimo.specs", "geronimo-jta_1.1_spec"),
             getBundle("org.apache.felix", "org.apache.felix.prefs"),
@@ -66,7 +67,6 @@ public class IntegrationTest extends AbstractIntegrationTest {
             getBundle("org.apache.servicemix.nmr", "org.apache.servicemix.nmr.spring"),
             getBundle("org.apache.servicemix.nmr", "org.apache.servicemix.nmr.osgi"),
             getBundle("org.apache.servicemix.document", "org.apache.servicemix.document"),
-            getBundle("org.apache.servicemix.jbi", "org.apache.servicemix.jbi.api"),
             getBundle("org.apache.servicemix.jbi", "org.apache.servicemix.jbi.runtime"),
             getBundle("org.apache.servicemix.jbi", "org.apache.servicemix.jbi.deployer"),
             getBundle("org.apache.servicemix.jbi", "org.apache.servicemix.jbi.osgi"),
