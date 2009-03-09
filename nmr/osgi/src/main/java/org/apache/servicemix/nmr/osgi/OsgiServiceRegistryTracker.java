2249b48f0fb315c9392c217e7efe13dda6e29441
Guillaume Nodet <gnodet@apache.org>
2009-03-09 17:05:04 +0000

    SMX4NMR-115: Do not use spring proxies for tracking endpoints and other NMR lists
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/nmr/trunk@751763 13f79535-47bb-0310-9956-ffa450edef68

--- nmr/osgi/src/main/java/org/apache/servicemix/nmr/osgi/OsgiServiceRegistryTracker.java
+++ nmr/osgi/src/main/java/org/apache/servicemix/nmr/osgi/OsgiServiceRegistryTracker.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.servicemix.nmr.osgi;
 
 import java.util.Map;
@@ -13,11 +29,9 @@ import org.springframework.osgi.context.BundleContextAware;
 import org.springframework.osgi.util.OsgiServiceReferenceUtils;
 
 /**
- * Created by IntelliJ IDEA.
- * User: gnodet
- * Date: Mar 9, 2009
- * Time: 3:05:46 PM
- * To change this template use File | Settings | File Templates.
+ * A simple spring factory bean that will create an OSGi service tracker and notify the configured registry
+ * when services are registered / unregistered in the OSGi registry.  This avoid using spring proxies which are
+ * not always needed especially in our case.
  */
 public class OsgiServiceRegistryTracker<T> implements BundleContextAware, InitializingBean, DisposableBean, ServiceTrackerCustomizer {
 
