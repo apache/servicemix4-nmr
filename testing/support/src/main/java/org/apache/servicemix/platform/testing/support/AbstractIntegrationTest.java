0f3d9cc57260dad44006e115fad9c82a39e404e8
Freeman Yue Fang <ffang@apache.org>
2010-08-19 06:43:02 +0000

    [SMX4NMR-220]platform/testing/support should use jaxp related api from system bundle, keep it same as we do currently in kit
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx4/nmr/trunk@987037 13f79535-47bb-0310-9956-ffa450edef68

--- testing/support/src/main/java/org/apache/servicemix/platform/testing/support/AbstractIntegrationTest.java
+++ testing/support/src/main/java/org/apache/servicemix/platform/testing/support/AbstractIntegrationTest.java
@@ -92,8 +92,6 @@ public class AbstractIntegrationTest extends AbstractConfigurableBundleCreatorTe
     protected String[] getTestFrameworkBundlesNames() {
         return new String[] {
             getBundle("org.apache.geronimo.specs", "geronimo-servlet_2.5_spec"),
-            getBundle("org.apache.servicemix.specs", "org.apache.servicemix.specs.jaxp-api-1.4"),
-            getBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.jaxp-ri"),
             getBundle("org.osgi", "org.osgi.core"),
             getBundle("org.osgi", "org.osgi.compendium"),
             getBundle("org.apache.felix", "org.apache.felix.configadmin"),
